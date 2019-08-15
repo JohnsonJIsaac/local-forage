3f3dd2157ca4e201fd73e509f1eaee45cd135def
Isaac <isaac.jj2016@gmail.com>
2019-08-15 16:43:07 -0500

    working app w/ math

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,47 +1,53 @@
-function getEmployeeData(cb) {
-    localforage.getItem("employeeData").then(function(results) {
+function getTrainData(cb) {
+    localforage.getItem("trainData").then(function(results) {
       cb(results || []);
     });
   }
-  function setEmployeeData(newEmployees, cb) {
-    localforage.setItem("employeeData", newEmployees).then(function() {
+  function setTrainData(newTrains, cb) {
+    localforage.setItem("trainData", newTrains).then(function() {
       cb();
     });
   }
-  function displayEmployees() {
-    getEmployeeData(function(recentEmployees) {
-      const mostRecentEmployees = document.getElementById("employee-table");
-      mostRecentEmployees.innerHTML = "";
-      for (let i = 0; i < recentEmployees.length; i++) {
-        const employee = recentEmployees[i];
+  function displayTrains() {
+    getTrainData(function(recentTrains) {
+      const mostRecentTrains = document.getElementById("train-table");
+      mostRecentTrains.innerHTML = "";
+      for (let i = 0; i < recentTrains.length; i++) {
+        const train = recentTrains[i];
+        console.log(train);
         const tr = document.createElement("tr");
-        const a = moment(employee.startdate);
-        const b = moment();
-        const c = b.diff(a, 'months');
-        tr.innerHTML = "<td>" + employee.name + "</td>"
-                      + "<td>" + employee.role + "</td>"
-                      + "<td>" + employee.startdate + "</td>"
-                      + "<td>" + c + "</td>"
-                      + "<td>" + employee.monthlyrate + "</td>"
-                      + "<td>" + employee.monthlyrate * c + "</td>"
-        mostRecentEmployees.append(tr);
+        const starttime = moment(train.firsttraintime, "HH:mm").subtract(1,"years");
+        console.log(starttime);
+        const difference = moment().diff(moment(starttime), "minutes");
+        console.log(difference);
+        const timeremaining = difference % parseInt(train.frequency);
+        console.log(timeremaining);
+        const nexttime = moment().add(timeremaining,"minutes").format("HH:mm");
+        console.log(nexttime);
+        tr.innerHTML = "<td>" + train.name + "</td>"
+                      + "<td>" + train.destination + "</td>"
+                      + "<td>" + train.frequency + "</td>"
+                      + "<td>" + nexttime + "</td>"
+                      + "<td>" + timeremaining + "</td>"
+                      
+        mostRecentTrains.append(tr);
       }
     });
   }
-  displayEmployees();
-  function handleNewEmployee(newEmployee) {
-    getEmployeeData(function(recentEmployee) {
-      recentEmployee.unshift(newEmployee);
+  displayTrains();
+  function handleNewTrain(newTrain) {
+    getTrainData(function(recentTrain) {
+      recentTrain.unshift(newTrain);
       //setEmployeeData(recentEmployee, function () {console.log("emplyee added... ")});
-      setEmployeeData(recentEmployee, displayEmployees);
+      setTrainData(recentTrain, displayTrains);
     });
   }
   // handleNewEmployee({ name: "test", role: "engineer", startdate: "1/1/2019", monthlyrate: 20 });
-  document.getElementById("empsubmit").addEventListener("click", function (event) {
+  document.getElementById("trainsubmit").addEventListener("click", function (event) {
     event.preventDefault();
-    const name = document.getElementById("eName").value.trim();
-    const role = document.getElementById("role").value.trim();
-    const startdate = document.getElementById("startdate").value;
-    const monthlyrate = document.getElementById("monthlyrate").value;
-    handleNewEmployee({ name:name , role: role, startdate: startdate, monthlyrate: monthlyrate });
+    const name = document.getElementById("trainname").value.trim();
+    const destination = document.getElementById("destination").value.trim();
+    const firsttraintime = document.getElementById("firsttraintime").value;
+    const frequency = document.getElementById("frequency").value;
+    handleNewTrain({ name:name , destination: destination, firsttraintime: firsttraintime, frequency: frequency });
   });
